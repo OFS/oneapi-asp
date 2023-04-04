4e5a9af54f1dfffde1dcc0affdb290899cddc8cb
DouglasGroen <77693638+DouglasGroen@users.noreply.github.com>
2023-04-04 08:34:17 -0700

    N6001: update license header in hardware/ and ase/ files (#9)

--- n6001/hardware/ofs_n6001_usm/build/scripts/ase-sim-compile.sh
+++ n6001/hardware/ofs_n6001_usm/build/scripts/ase-sim-compile.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# (C) 2017 Intel Corporation. All rights reserved.
-# Your use of Intel Corporation's design tools, logic functions and other
-# software and tools, and its AMPP partner logic functions, and any output
-# files any of the foregoing (including device programming or simulation
-# files), and any associated documentation or information are expressly subject
-# to the terms and conditions of the Intel Program License Subscription
-# Agreement, Intel MegaCore Function License Agreement, or other applicable
-# license agreement, including, without limitation, that your use is for the
-# sole purpose of programming logic devices manufactured by Intel and sold by
-# Intel or its authorized distributors.  Please refer to the applicable
-# agreement for further details.
+# Copyright 2022 Intel Corporation
+# SPDX-License-Identifier: MIT
 
 echo "Start of sim_compile.sh"
 
