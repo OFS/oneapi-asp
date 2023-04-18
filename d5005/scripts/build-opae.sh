8ed1a74c2e26fe1dd7f13751ed93127d737ca26a
Groen, Douglas <douglas.groen@intel.com>
2023-04-18 11:46:19 -0700

    use opae-sim release branch, too

--- d5005/scripts/build-opae.sh
+++ d5005/scripts/build-opae.sh
@@ -121,7 +121,7 @@ if [ -n "$OFS_OCL_ENV_ENABLE_ASE" ]; then
     fi
     # Default branch to 'master' if the branch variable is not set
     if [ -z "${OPAESIM_REPO_BRANCH}" ]; then
-        OPAESIM_REPO_BRANCH="master"
+        OPAESIM_REPO_BRANCH="release/2.5.0"
     fi
     # Default location to clone is $BUILD_PREFIX/opae-sim
     if [ -z "${OPAESIM_PATH}" ]; then
