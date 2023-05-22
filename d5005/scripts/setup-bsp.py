31e6d1c97fa3785f29c8e0cc24200448ba8c0d3e
Groen, Douglas <douglas.groen@intel.com>
2023-05-22 15:50:20 -0700

    clean up previous pr-build-template folder and copy new one

--- d5005/scripts/setup-bsp.py
+++ d5005/scripts/setup-bsp.py
@@ -150,6 +150,10 @@ def setup_bsp(bsp_root, env_vars, bsp, verbose):
     bsp_qsf_dir = os.path.join(bsp_dir, 'build')
 
     print("bsp_qsf_dir is %s\n" % bsp_qsf_dir)
+    
+    #preserve the pr-build-template folder
+    delete_and_mkdir(os.path.join(bsp_dir, '../../pr_build_template'))
+    copy_glob(deliverable_dir, os.path.join(bsp_dir, '../../'),verbose)
 
     # copy the FIM FME information text files
     copy_glob(os.path.join(deliverable_hwlib_dir, 'fme*.txt'), bsp_qsf_dir, verbose)
