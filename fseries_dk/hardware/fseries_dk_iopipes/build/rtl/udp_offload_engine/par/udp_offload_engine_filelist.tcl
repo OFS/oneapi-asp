da83c02ded50498ce5d808b86486c4fb601915f6
Groen, Douglas <douglas.groen@intel.com>
2023-08-03 16:47:17 -0700

    misc cleanup

--- fseries_dk/hardware/fseries_dk_iopipes/build/rtl/udp_offload_engine/par/udp_offload_engine_filelist.tcl
+++ fseries_dk/hardware/fseries_dk_iopipes/build/rtl/udp_offload_engine/par/udp_offload_engine_filelist.tcl
@@ -16,6 +16,4 @@ set_global_assignment -name SYSTEMVERILOG_FILE "${THIS_DIR}/../rtl/udp_oe_pkg.sv
 set_global_assignment -name SYSTEMVERILOG_FILE "${THIS_DIR}/../rtl/udp_oe_interfaces.sv"
 set_global_assignment -name IP_FILE "${THIS_DIR}/../ip/asp_dcfifo.ip"
 
-#qexec "qsys-generate /nfs/site/disks/swuser_work_dgroen/work/ofs-io-channels-0/071823/oneapi-asp/n6001/hardware/ofs_n6001/build/rtl/udp_offload_engine/ip/asp_dcfifo.ip --quartus-project=ofs_top --output-directory=/nfs/site/disks/swuser_work_dgroen/work/ofs-io-channels-0/071823/oneapi-asp/n6001/hardware/ofs_n6001/build/rtl/udp_offload_engine/ip/ --synthesis"
-
 set_global_assignment -name SDC_FILE "${THIS_DIR}/shim_udp_offload_engine.sdc"
