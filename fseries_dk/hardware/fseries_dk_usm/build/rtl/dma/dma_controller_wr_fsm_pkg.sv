fb2b1fa39546959962f9c8bca177d5561ac5d147
Groen, Douglas <douglas.groen@intel.com>
2023-08-03 16:42:17 -0700

    create fseries-dk variants

---
+++ fseries_dk/hardware/fseries_dk_usm/build/rtl/dma/dma_controller_wr_fsm_pkg.sv
@@ -0,0 +1,14 @@
+// Copyright 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+//
+
+//DMA controller write-FSM state enumerations
+package dma_controller_wr_fsm_pkg;
+
+    typedef enum {  WIDLE,
+                    WAIT_FOR_FIRST_WRITE,
+                    WRITE,
+                    WRITE_MAGIC_NUM,
+                    WXXX } wr_state_e;
+
+endpackage
