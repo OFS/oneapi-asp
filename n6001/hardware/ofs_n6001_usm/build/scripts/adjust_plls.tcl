04f0364ec498aa9f0c6a5eaf27c2f9293c275a58
DouglasGroen <77693638+DouglasGroen@users.noreply.github.com>
2023-08-14 09:10:51 -0700

    N6001: adapt to FIM's kernel-clock hierarchy name change (#70)
    A recent FIM change altered the hierarchy of the user/kernel clock, causing adjust_plls.tcl (and associated kernel-clock timing constraints) to fail. This work-around fixes takes that hierarchy change into account. A proper future fix should leverage the scripts within ofs_partial_reconfig from the FIM.

--- n6001/hardware/ofs_n6001_usm/build/scripts/adjust_plls.tcl
+++ n6001/hardware/ofs_n6001_usm/build/scripts/adjust_plls.tcl
@@ -9,9 +9,9 @@ package require ::quartus::flow
 
 # Definitions
 #normal/slow clock
-set k_clk_name "afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk1"
+set k_clk_name "afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk1"
 #double/fast clock
-set k_clk2x_name "afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk0"
+set k_clk2x_name "afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk0"
 set k_fmax -1
 set jitter_compensation 0.01
 set unused_clock_freq 10000
@@ -433,8 +433,8 @@ while { $timing_clean == 0 && $timing_loop_cnt <= $max_num_loops} {
     set sdcfile   [open "user_clock.sdc" w]
     puts $sdcfile "#updated user-clock.sdc assignments based on fmax from previous fit attempt."
     puts $sdcfile "puts \"Updated user-clock constraints based on fmax from previous fit attempt.\" "
-    puts $sdcfile "create_clock -name {afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk0} -period $period2 \[get_pins {afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0|tennm_pll|outclk[1]}]"
-    puts $sdcfile "create_clock -name {afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk1} -period $period \[get_pins {afu_top|port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0|tennm_pll|outclk[2]}] "
+    puts $sdcfile "create_clock -name {afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk0} -period $period2 \[get_pins {afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0|tennm_pll|outclk[1]}]"
+    puts $sdcfile "create_clock -name {afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0_outclk1} -period $period \[get_pins {afu_top|pg_afu.port_gasket|user_clock|qph_user_clk|qph_user_clk_iopll|iopll_0|tennm_pll|outclk[2]}] "
     puts $sdcfile "$disable_mpw_sdccmd"
     close $sdcfile
     
