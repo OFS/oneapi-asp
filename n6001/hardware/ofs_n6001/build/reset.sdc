1472231e5d9f520cdd23b94243c71e6e252483c8
Groen, Douglas <douglas.groen@intel.com>
2023-09-12 10:16:58 -0700

    misc

--- n6001/hardware/ofs_n6001/build/reset.sdc
+++
@@ -1,3 +0,0 @@
-# Copyright 2022 Intel Corporation
-# SPDX-License-Identifier: MIT
-set_false_path -to [get_pins -compatibility_mode -nocase *|alt_rst_sync_uq1|altera_reset_synchronizer_int_chain*|d]
--- n6001/hardware/ofs_n6001_usm_iopipes/build/scripts/entry.tcl
+++ n6001/hardware/ofs_n6001_usm_iopipes/build/scripts/entry.tcl
@@ -15,7 +15,6 @@ switch $tcl_platform(platform) {
       set revision_name import
     }
     post_message "Compiling revision $revision_name"
-    #qexec "bash build/scripts/run.sh $revision_name"
     set FIM_BOARD_PATH [glob  -directory fim_platform/build/syn/board/ -type d *]
     post_message "FIM_BOARD_PATH is $FIM_BOARD_PATH"
     if {[catch {qexec "bash $FIM_BOARD_PATH/syn_top/scripts/run.sh $revision_name"} result]} {
