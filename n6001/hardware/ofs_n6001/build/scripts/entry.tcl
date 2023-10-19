c18756a66e266fd1cdf1edf60f8f92b353b7392e
Groen, Douglas <douglas.groen@intel.com>
2023-10-18 21:44:25 -0700

    misc

--- n6001/hardware/ofs_n6001/build/scripts/entry.tcl
+++ n6001/hardware/ofs_n6001/build/scripts/entry.tcl
@@ -1,7 +1,11 @@
 # Copyright 2022 Intel Corporation
 # SPDX-License-Identifier: MIT
+set script_path [ file dirname [ file normalize [ info script ] ] ]
 
-source syn_top_relpath.tcl
+#The path to the syn_top directory from the FIM-build was written to 
+#the syn_top_relpath.tcl file during the execution of setup-bsp.py.
+#We use it here to find the location to do the Quartus build.
+source $script_path/syn_top_relpath.tcl
 
 #Full compiles are not supported on Windows
 switch $tcl_platform(platform) {
